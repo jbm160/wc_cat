1cf3aae5a55645b78bc38f0da578c71496b1e9fa
jbm160 <jbm160@hotmail.com>
2015-12-08 22:08:46 -0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,15 +2,18 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+
+$html = scraperwiki::scrape("http://72.21.92.20/category/wdc/woodcraft.aspx?sort=priceD");
+
 //
 // // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
+$dom = new simple_html_dom();
+$dom->load($html);
 // print_r($dom->find("table.list"));
 //
 // // Write out to the sqlite database using scraperwiki library
