a770f739ed7a34dbcece88303e0cfa11a8b48d93
jbm160 <jbm160@hotmail.com>
2015-12-11 06:35:19 -0600

    Update productlist.php

--- productlist.php
+++ productlist.php
@@ -35,7 +35,7 @@ function getProducts($u){
   $path = "";
   $d = new simple_html_dom();
   $d->load(scraperwiki::scrape($u));
-//echo "Loaded URL: " . $u . "\n";
+echo "Loaded URL: " . $u . "\n";
   if ($S2Prod = count($d->find('span[class=S2Product]')) && $S2Prod > 0) {
   	foreach ($S2Prod as $p) {
   		$sku = trim($p->find('div[class=S2ProductSku]',0)->innertext,"# ";
